b38098bacbcd118daf4631f0683fbcbfd22bb7ba
MarysolHelena <marysolgomesm@gmail.com>
2023-10-26 19:32:41 -0300

    rotas request 26-10

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
 /* eslint-disable prettier/prettier */
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
 
-import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+const app = fastify();
 
-const app = fastify()
-const prisma = new PrismaClient()
+app.register(cors, { origin: true });
+app.register(memoriesRoutes);
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+    console.log("🚀 HTTP server running on http://localhost:3333");
+  });
